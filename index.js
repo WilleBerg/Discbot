153424bcc58f28ad79504fe1c863c97333116adb
WilleBerg <bergw@student.chalmers.se>
2022-03-06 17:05:27 +0100

    it works now

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const fs = require('fs');
 const { Client, Collection, Intents, VoiceChannel } = require('discord.js');
 const { token, prefix } = require('./config.json');
+global.AbortController = require("node-abort-controller").AbortController;
 const ytdl = require('ytdl-core');
+const ply = require('play-dl');
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_VOICE_STATES] });
 const {
 	AudioPlayerStatus,
@@ -42,6 +44,7 @@ client.on('messageCreate', async message => {
 
 
     if (message.content.startsWith(`${prefix}play`)) {
+        
         execute(message, serverQueue);
         return;
     } else if (message.content.startsWith(`${prefix}skip`)) {
@@ -50,18 +53,23 @@ client.on('messageCreate', async message => {
     } else if (message.content.startsWith(`${prefix}stop`)) {
         stop(message, serverQueue);
         return;
-    } else if (message.content.startsWith(`${prefix}queue`)){
-        let songList = queue.get(message.guild.id);
-        if(songList != undefined){
-          let songString;
-          for(const song in songList.songs) {
-            songString += "\n" + song.title;
+    } else if (message.content.startsWith(`${prefix}q`)){
+        if(serverQueue != undefined){
+          let songString = "";
+          for(let i = 0; i < serverQueue.songs.length; i++) {
+            if(serverQueue.songs[i].title != undefined) songString += "\n**" + serverQueue.songs[i].title + "**";
           }
-          message.channel.send("Song queue: \n" + songString);
+          message.channel.send("Song queue:" + songString);
         } else {
           message.channel.send("Song queue empty or bot is broken <:Sadge:852903092315357204>");
         }
         
+    } else if(message.content.startsWith(`${prefix}raindance`)){
+      message.content = "!play https://www.youtube.com/watch?v=aMTLs4qfJtI";
+      execute(message, serverQueue);
+    } else if(message.content.startsWith(`${prefix}soviet`)){
+      message.content = "!play https://www.youtube.com/watch?v=U06jlgpMtQs";
+      execute(message, serverQueue);
     } else {
         message.channel.send("You need to enter a valid command!");
     }
@@ -108,8 +116,7 @@ async function execute(message, serverQueue) {
         title: songInfo.videoDetails.title,
         url: songInfo.videoDetails.video_url,
     };
-
-    console.log("im here");
+    
     if (!serverQueue) {
         const queueContruct = {
         textChannel: message.channel,
@@ -117,7 +124,9 @@ async function execute(message, serverQueue) {
         connection: null,
         songs: [],
         volume: 5,
-        playing: true
+        playing: true,
+        player: null,
+        guild: message.guild
         };
 
         queue.set(message.guild.id, queueContruct);
@@ -143,14 +152,23 @@ async function execute(message, serverQueue) {
     }
   }
 
-  function skip(message, serverQueue) {
+  async function skip(message, serverQueue) {
     if (!message.member.voice.channel)
       return message.channel.send(
         "You have to be in a voice channel to stop the music!"
       );
     if (!serverQueue)
       return message.channel.send("There is no song that I could skip!");
-    serverQueue.connection.destroy();
+    let rec = await getNextResource(serverQueue);
+    if(rec != undefined) {
+      serverQueue.player.play(rec);
+      serverQueue.textChannel.send(`Skipping song.\nNext up is **${serverQueue.songs[0].title}**<a:pepeJAM:852902332164603935>`);
+    }
+    else {
+      serverQueue.connection.destroy();
+      serverQueue.textChannel.send(`Queue empty!\nIm leaving<a:peepoLeave:852903257256755250>`);
+      queue.delete(serverQueue.guild.id);
+    }
   }
   
   function stop(message, serverQueue) {
@@ -162,11 +180,12 @@ async function execute(message, serverQueue) {
     if (!serverQueue)
       return message.channel.send("There is no song that I could stop!");
       
-    serverQueue.songs = [];
-    serverQueue.connection.destroy();
+      serverQueue.connection.destroy();
+      queue.delete(serverQueue.voiceChannel.guild.id);
+      return message.channel.send("Stopping!\nQueue has been emptied.");
   }
   
-  function play(guild, song, connection) {
+  async function play(guild, song, connection) {
     const serverQueue = queue.get(guild.id);
     if (!song) {
       serverQueue.voiceChannel.leave();
@@ -174,22 +193,54 @@ async function execute(message, serverQueue) {
       return;
     }
     
-    const stream = ytdl(song.url, {filter: 'audioonly'});
-    const resource = createAudioResource(stream, { inputType: StreamType.Arbitrary });
+    const stream = await ply.stream(song.url);
+    const resource = createAudioResource(stream.stream, { inputType: stream.type });
     const player = createAudioPlayer();
 
-    try {
-      player.play(resource);
-      connection.subscribe(player);
-      
-      player.on(AudioPlayerStatus.Idle, () => connection.destroy());
-      serverQueue.textChannel.send(`Start playing: **${song.title}**`);
-    } catch(err) {
-      console.error(err);
-      serverQueue.textChannel.send(`mb g, bot broken @sylt @sylt`);
-    }
-  }
+    player.play(resource);
+    connection.subscribe(player);
+    serverQueue.player = player;
 
+
+
+    player.on(AudioPlayerStatus.Idle, async () => {
+      const rec = await getNextResource(serverQueue);
+      
+      if(rec != undefined) {
+        player.play(rec); 
+        serverQueue.textChannel.send(`Now playing: **${serverQueue.songs[0].title}**<a:pepeJAM:852902332164603935>`);
+      }
+      else {
+        connection.destroy();
+        serverQueue.textChannel.send(`Queue empty!\nIm leaving<a:peepoLeave:852903257256755250>`);
+        queue.delete(guild.id);
+      }
+      
+    });
+    player.on('error', error => {
+      console.error(error.message);
+      connection.destroy();
+      serverQueue.textChannel.send(`Error: **${error.message}** <:Sadge:852903092315357204> \nDestroying connection`);
+      queue.delete(serverQueue.voiceChannel.guild.id);
+      return;
+    });
+    serverQueue.textChannel.send(`Start playing: **${song.title}**<a:pepeJAM:852902332164603935>`);
+
+  }
+  async function getNextResource(serverQueue){
+    console.log("shifting queue...");
+    serverQueue.songs.shift();
+    console.log("creating stream...");
+    if(serverQueue.songs[0] != undefined) {
+      console.log("inside if statement ffs");
+      const stream = await ply.stream(serverQueue.songs[0].url);
+      console.log("creating audio resource...");
+      const resource = createAudioResource(stream.stream, { inputType: stream.type });
+      console.log("returning resource, will try playing...");
+      return resource;
+    } else return undefined;
+    
+  }
 
 
 client.login(token);
