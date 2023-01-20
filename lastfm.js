7538bcc131cdfcd71922aa46a9db77a61661b8ab
WilleBerg <william.berg2@gmail.com>
2023-01-20 16:49:03 +0100

    now scrobbles songs as batches, much much faster

--- lastfm.js
+++ lastfm.js
@@ -43,58 +43,98 @@ async function scrobbleSongs(
     timestamps,
     sessionKey
 ) {
-    log(`\n\n\n\n\n`);
-    log("scrobbleSongs called");
-    log(`songNames: ${songNames}`);
-    log(`artistNames: ${artistNames}`);
-    log(`albums: ${albums}`);
-    log(`timestamps: ${timestamps}`);
-    log(`sessionKey: ${sessionKey}`);
-    log(`\n\n\n\n\n`);
-    var auth_sig = "album[";
+    var msg = "";
+    msg += (`\n\n\n\n\n`);
+    msg += ("scrobbleSongs called\n");
+    msg += (`songNames: ${songNames}\n`);
+    msg += (`artistNames: ${artistNames}\n`);
+    msg += (`albums: ${albums}\n`);
+    msg += (`timestamps: ${timestamps}\n`);
+    msg += (`sessionKey: ${sessionKey}\n`);
+    var tmpstring = "";
+    var albumList = [];
     for (var i = 0; i < albums.length; i++) {
-        auth_sig += albums[i];
-        if (i != albums.length - 1) auth_sig += ",";
-        else auth_sig += "]";
+        tmpstring += "album";
+        tmpstring += `[${i}]`;
+        tmpstring += albums[i]; 
+        albumList.push(tmpstring);
+        tmpstring = "";
     }
-    auth_sig += `api_key${LAST_FM_API_KEY}`;
-    auth_sig += "artist[";
+    var artistList = [];
     for (var i = 0; i < artistNames.length; i++) {
-        auth_sig += artistNames[i];
-        if (i != artistNames.length - 1) auth_sig += ",";
-        else auth_sig += "]";
+        tmpstring += "artist";
+        tmpstring += `[${i}]`;
+        tmpstring += artistNames[i]; 
+        artistList.push(tmpstring);
+        tmpstring = "";
     }
-    auth_sig += `methodtrack.scrobblesk${sessionKey}`;
-    auth_sig += "timestamp[";
+    var timestampList = [];
     for (var i = 0; i < timestamps.length; i++) {
-        auth_sig += timestamps[i];
-        if (i != timestamps.length - 1) auth_sig += ",";
-        else auth_sig += "]";
+        tmpstring += "timestamp";
+        tmpstring += `[${i}]`;
+        tmpstring += timestamps[i]; 
+        timestampList.push(tmpstring);
+        tmpstring = "";
     }
-    auth_sig += "track[";
+    var trackList = [];
     for (var i = 0; i < songNames.length; i++) {
-        auth_sig += songNames[i];
-        if (i != songNames.length - 1 ) auth_sig += ",";
-        else auth_sig += "]";
+        tmpstring += "track";
+        tmpstring += `[${i}]`;
+        tmpstring += songNames[i]; 
+        trackList.push(tmpstring);
+        tmpstring = "";
     }
-    // add secret to end
-    auth_sig += secret;
-    log(auth_sig);
-    //auth_sig = encodeURI(auth_sig);
+    var auth_sig = "";
+    albumList.sort(); 
+    artistList.sort();
+    timestampList.sort();
+    trackList.sort();
+
+    var albumString = "";
+    var artistString = "";
+    var timestampString = "";
+    var trackString = "";
+    
+    for (var i = 0; i < albumList.length; i++) {
+        albumString += albumList[i];
+        artistString+= artistList[i];
+        timestampString += timestampList[i];
+        trackString += trackList[i];
+    }
+    auth_sig = `${albumString}api_key${LAST_FM_API_KEY}${artistString}methodtrack.scrobblesk${sessionKey}${timestampString}${trackString}${secret}`;
+
+    msg += (`auth_sig: ${auth_sig}\n`);
     var auth_sig_md5Hex = md5(auth_sig);
-    log(auth_sig_md5Hex);
-    try {
-        var url = `${LAST_FM_API_BASE}?method=track.scrobble&api_key=${LAST_FM_API_KEY}&sk=${sessionKey}&artist=[${artistNames}]&track=[${songNames}]&album=[${albums}]&timestamp=[${timestamps}]&format=json&api_sig=${auth_sig_md5Hex}`;
-        //url = encodeURI(url);
-        log(url);
+    msg += (`auth_sig_md5Hex: ${auth_sig_md5Hex}\n`);
+    var urlTrackString = "";
+    var urlArtistString = "";
+    var urlAlbumString = "";
+    var urlTimestampString = "";
+    for (var i = 0; i < songNames.length; i++) {
+        urlTrackString += `track[${i}]=${songNames[i]}`;
+        if (i != songNames.length - 1) urlTrackString += "&";
+        urlArtistString += `artist[${i}]=${artistNames[i]}`;
+        if (i != songNames.length - 1) urlArtistString += "&";
+        urlAlbumString += `album[${i}]=${albums[i]}`;
+        if (i != songNames.length - 1) urlAlbumString += "&";
+        urlTimestampString += `timestamp[${i}]=${timestamps[i]}`;
+        if (i != songNames.length - 1) urlTimestampString += "&";
+    }
+    try{
+        var url = `${LAST_FM_API_BASE}?method=track.scrobble&api_key=${LAST_FM_API_KEY}&sk=${sessionKey}&${urlArtistString}&${urlTrackString}&${urlAlbumString}&${urlTimestampString}&format=json&api_sig=${auth_sig_md5Hex}`;
+        url = encodeURI(url);
+        msg += (`url: ${url}\n`);
         const response = await fetch(url, { method: "POST" });
         const data = await response.json();
-        return data;
+        log(msg)
+        return 1;
     } catch (error) {
-        log(`Error from scrobbleSongs: ${error}`);
+        log(`Error from testScrobbles: ${error}`);
         return "error";
     }
 }
+
+// Will stay as a memory of the time I spent trying to figure out how to scrobble a batch of songs
 async function testScrobbles(
     songNames,
     artistNames,
@@ -116,79 +156,153 @@ async function testScrobbles(
     msg += (`timestamps: ${timestamps}\n`);
     msg += (`sessionKey: ${sessionKey}\n`);
     msg += (`isReverse: ${isReverse} , hasBracketsInMD5: ${hasBracketsInMD5} , hasBracketsInLink: ${hasBracketsInLink} , ismd5Encoded: ${ismd5Encoded} , isUrlEncoded: ${isUrlEncoded}\n`);
-    if (hasBracketsInMD5) var auth_sig = "album[";
+    if (hasBracketsInMD5) var auth_sig = "";
     else var auth_sig = "album";
+    var tmpstring = "";
+    var albumList = [];
     if (isReverse) {
         for (var i = albums.length - 1; i >= 0; i--) {
+            auth_sig += "album";
+            if (hasBracketsInMD5) auth_sig += `[${i}]`;
             auth_sig += albums[i];
-            if (i != 0 && hasBracketsInMD5) auth_sig += ",";
-            else if(hasBracketsInMD5) auth_sig += "]";
         }
     } else {
         for (var i = 0; i < albums.length; i++) {
-            auth_sig += albums[i];
-            if (i != albums.length - 1 && hasBracketsInMD5) auth_sig += ",";
-            else if(hasBracketsInMD5) auth_sig += "]";
+            tmpstring += "album";
+            if (hasBracketsInMD5) tmpstring += `[${i}]`;
+            tmpstring += albums[i]; 
+            albumList.push(tmpstring);
+            tmpstring = "";
         }
     }
-    auth_sig += `api_key${LAST_FM_API_KEY}`;
-    if (hasBracketsInMD5) auth_sig += "artist[";
+    auth_sig += tmpstring + `api_key${LAST_FM_API_KEY}`;
+    tmpstring = "";
+    if (hasBracketsInMD5) auth_sig += "";
+     
     else auth_sig += "artist";
+    var artistList = [];
     if (isReverse) {
         for (var i = artistNames.length - 1; i >= 0; i--) {
+            auth_sig += "artist";
+            if (hasBracketsInMD5) auth_sig += `[${i}]`;
             auth_sig += artistNames[i];
-            if (i != 0 && hasBracketsInMD5) auth_sig += ",";
-            else if(hasBracketsInMD5) auth_sig += "]";
         }
     }
     else {
         for (var i = 0; i < artistNames.length; i++) {
-            auth_sig += artistNames[i];
-            if (i != artistNames.length - 1 && hasBracketsInMD5) auth_sig += ",";
-            else if(hasBracketsInMD5) auth_sig += "]";
+            tmpstring += "artist";
+            if (hasBracketsInMD5) tmpstring += `[${i}]`;
+            tmpstring += artistNames[i]; 
+            artistList.push(tmpstring);
+            tmpstring = "";
         }
     }
-    auth_sig += `methodtrack.scrobblesk${sessionKey}`;
-    if (hasBracketsInMD5) auth_sig += "timestamp[";
+    auth_sig += tmpstring + `methodtrack.scrobblesk${sessionKey}`;
+    tmpstring = "";
+    if (hasBracketsInMD5) auth_sig += "";
     else auth_sig += "timestamp";
+    var timestampList = [];
     if (isReverse) {
         for (var i = timestamps.length - 1; i >= 0; i--) {
+            auth_sig += "timestamp";
+            if (hasBracketsInMD5) auth_sig += `[${i}]`;
             auth_sig += timestamps[i];
-            if (i != 0 && hasBracketsInMD5) auth_sig += ",";
-            else if(hasBracketsInMD5) auth_sig += "]";
         }
     }
     else {
         for (var i = 0; i < timestamps.length; i++) {
-            auth_sig += timestamps[i];
-            if (i != timestamps.length - 1 && hasBracketsInMD5) auth_sig += ",";
-            else if(hasBracketsInMD5) auth_sig += "]";
+            tmpstring += "timestamp";
+            if (hasBracketsInMD5) tmpstring += `[${i}]`;
+            tmpstring += timestamps[i]; 
+            timestampList.push(tmpstring);
+            tmpstring = "";
         }
     }
-    if (hasBracketsInMD5) auth_sig += "track[";
+    auth_sig += tmpstring;
+    tmpstring = "";
+    if (hasBracketsInMD5) auth_sig += "";
     else auth_sig += "track";
+    var trackList = [];
     if (isReverse) {
         for (var i = songNames.length - 1; i >= 0; i--) {
+            auth_sig += "track";
+            if (hasBracketsInMD5) auth_sig += `[${i}]`;
             auth_sig += songNames[i];
-            if (i != 0 && hasBracketsInMD5) auth_sig += ",";
-            else if(hasBracketsInMD5) auth_sig += "]";
         }
     }
     else {
         for (var i = 0; i < songNames.length; i++) {
-            auth_sig += songNames[i];
-            if (i != songNames.length - 1 && hasBracketsInMD5) auth_sig += ",";
-            else if(hasBracketsInMD5) auth_sig += "]";
+            tmpstring += "track";
+            if (hasBracketsInMD5) tmpstring += `[${i}]`;
+            tmpstring += songNames[i]; 
+            trackList.push(tmpstring);
+            tmpstring = "";
         }
     }
-    auth_sig += secret;
+    auth_sig += tmpstring + secret;
+    auth_sig = "";
+    log("albumList: " + albumList);
+    albumList.sort(); 
+    log("albumList: " + albumList);
+    artistList.sort();
+    timestampList.sort();
+    trackList.sort();
+
+    var albumString = "";
+    var artistString = "";
+    var timestampString = "";
+    var trackString = "";
+    
+    for (var i = 0; i < albumList.length; i++) {
+        albumString += albumList[i];
+        artistString+= artistList[i];
+        timestampString += timestampList[i];
+        trackString += trackList[i];
+    }
+    auth_sig = `${albumString}api_key${LAST_FM_API_KEY}${artistString}methodtrack.scrobblesk${sessionKey}${timestampString}${trackString}${secret}`;
+
+
     msg += (`auth_sig: ${auth_sig}\n`);
     if (ismd5Encoded) auth_sig = encodeURI(auth_sig);
     var auth_sig_md5Hex = md5(auth_sig);
     msg += (`auth_sig_md5Hex: ${auth_sig_md5Hex}\n`);
+    var urlTrackString = "";
+    var urlArtistString = "";
+    var urlAlbumString = "";
+    var urlTimestampString = "";
+    if (hasBracketsInLink) {
+        if(isReverse) {
+            for (var i = songNames.length - 1; i >= 0; i--) {
+                urlTrackString += `track[${i}]=${songNames[i]}`;
+                if (i != 0) urlTrackString += "&";
+                urlArtistString += `artist[${i}]=${artistNames[i]}`;
+                if (i != 0) urlArtistString += "&";
+                urlAlbumString += `album[${i}]=${albums[i]}`;
+                if (i != 0) urlAlbumString += "&";
+                urlTimestampString += `timestamp[${i}]=${timestamps[i]}`;
+                if (i != 0) urlTimestampString += "&";
+            }
+        } else {
+            for (var i = 0; i < songNames.length; i++) {
+                urlTrackString += `track[${i}]=${songNames[i]}`;
+                if (i != songNames.length - 1) urlTrackString += "&";
+                urlArtistString += `artist[${i}]=${artistNames[i]}`;
+                if (i != songNames.length - 1) urlArtistString += "&";
+                urlAlbumString += `album[${i}]=${albums[i]}`;
+                if (i != songNames.length - 1) urlAlbumString += "&";
+                urlTimestampString += `timestamp[${i}]=${timestamps[i]}`;
+                if (i != songNames.length - 1) urlTimestampString += "&";
+            }
+        }
+    } else {
+        urlTrackString = `track=${songNames}`;
+        urlArtistString = `artist=${artistNames}`;
+        urlAlbumString = `album=${albums}`;
+        urlTimestampString = `timestamp=${timestamps}`;
+    }
     try{
         if (hasBracketsInLink) {
-            var url = `${LAST_FM_API_BASE}?method=track.scrobble&api_key=${LAST_FM_API_KEY}&sk=${sessionKey}&artist=[${artistNames}]&track=[${songNames}]&album=[${albums}]&timestamp=[${timestamps}]&format=json&api_sig=${auth_sig_md5Hex}`;
+            var url = `${LAST_FM_API_BASE}?method=track.scrobble&api_key=${LAST_FM_API_KEY}&sk=${sessionKey}&${urlArtistString}&${urlTrackString}&${urlAlbumString}&${urlTimestampString}&format=json&api_sig=${auth_sig_md5Hex}`;
         } else {
             var url = `${LAST_FM_API_BASE}?method=track.scrobble&api_key=${LAST_FM_API_KEY}&sk=${sessionKey}&artist=${artistNames}&track=${songNames}&album=${albums}&timestamp=${timestamps}&format=json&api_sig=${auth_sig_md5Hex}`;
         }
@@ -196,10 +310,12 @@ async function testScrobbles(
         msg += (`url: ${url}\n`);
         const response = await fetch(url, { method: "POST" });
         const data = await response.json();
+        log(msg);
+        log(JSON.stringify(data));
         if (data["error"] != undefined && data.error == 13) {
             return 0;
         } else {
-            log(msg)
+            //log(msg);
             return data;
         }
     } catch (error) {
