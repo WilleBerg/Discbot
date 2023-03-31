46963eb157d69972b9134e45dfa3b923502fd07d
WilleBerg <william.berg2@gmail.com>
2023-03-31 13:09:40 +0200

    ignore

--- lastfm.js
+++ lastfm.js
@@ -110,6 +110,7 @@ async function scrobbleSongs(
     auth_sig = `${albumString}api_key${LAST_FM_API_KEY}${artistString}methodtrack.scrobblesk${sessionKey}${timestampString}${trackString}${secret}`;
     msg += `auth_sig: ${auth_sig}\n`;
     auth_sig = changeToUtf8(auth_sig);
+    msg += `auth_sig post utf8 fix: ${auth_sig}\n`;
     var auth_sig_md5Hex = md5(auth_sig);
     msg += `auth_sig_md5Hex: ${auth_sig_md5Hex}\n`;
     var urlTrackString = "";
